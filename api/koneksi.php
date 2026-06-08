051402227cab58f2a8c443d5bb6edcdb28a9e7ab
fikri fikri <davaarvarabi9@gmail.com>
2026-06-08 22:34:33 +0700

    revisi backend

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,14 +1,15 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
-header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Origin, Accept");
 
+// Pastikan untuk menangani OPTIONS preflight dengan benar
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
 
+header("Content-Type: application/json; charset=UTF-8");
 // Konfigurasi menggunakan Environment Variables dari Railway
 $host = getenv('MYSQLHOST');
 $user = getenv('MYSQLUSER');
