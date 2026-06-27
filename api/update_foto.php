16b7c71215efe484761e7eda180badd4e07f2972
fikri fikri <davaarvarabi9@gmail.com>
2026-06-27 14:15:24 +0700

    Foto profil

---
+++ api/update_foto.php
@@ -0,0 +1,22 @@
+<?php
+include 'koneksi.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $iduser = $_POST['iduser'] ?? '';
+    $foto_base64 = $_POST['foto_profil'] ?? ''; // Menerima teks Base64 dari Flutter/Android
+
+    if (empty($iduser) || empty($foto_base64)) {
+        echo json_encode(["status" => false, "message" => "Data tidak lengkap!"]);
+        exit();
+    }
+
+    // Update string foto ke database
+    $query = "UPDATE user SET foto_profil = '$foto_base64' WHERE iduser = '$iduser'";
+    
+    if (mysqli_query($koneksi, $query)) {
+        echo json_encode(["status" => true, "message" => "Foto profil berhasil diperbarui!"]);
+    } else {
+        echo json_encode(["status" => false, "message" => "Gagal memperbarui database: " . mysqli_error($koneksi)]);
+    }
+}
+?>
